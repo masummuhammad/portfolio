199ac5b564461046e446024bb8d7ff6495fe4fa0
Muhammad Masum <48872682+masummuhammad@users.noreply.github.com>
2022-05-10 21:24:09 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import "@fontsource/roboto-mono";
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 const Index=()=>{
   
   return(
@@ -10,14 +11,14 @@ const Index=()=>{
        <title>Muhammad Masum</title>
       
      </Head>
-     <img alt="muhammad masum's photo" src="portfolio_head.jpg" style={{height:'40vh',width:'auto'}}/>
+     <Image alt="muhammad masum's photo" src="portfolio_head.jpg" style={{height:'40vh',width:'auto'}}/>
     <p>Hi, I am Muhammad Masum. I have started my programming journey 5 years ago by a book written by tamim shahriar subeen.It was a 
       book on c programming language. I was very passionate about to learn how web application works. I 
       started to learn html, css, javascript, nodejs , sql ,mongodb, express , RESTapi,react, socket.io etc. In 2021, I learnt
       typescript, graphql , postgresql ,typeorm, nextjs etc and I created <a href='https://tribindu.vercel.app' style={{color:'blue',textDecoration:'none'}}>tribindu</a>.Besides, I can write code in python ,java ,c++ and c++
        is my favourite.
       At the beginning I was a self-taught programmer,then I moved to obtain a degree on computer science.
-      I have participated in some online programming contest at codeforces dot com and solved 150+ URI online judge problems.
+      I have participated in some online programming contests at codeforces dot com and solved 150+ URI online judge problems.
       My next step will be solving problems at leetcode.
       I love to play video games, listen music and to travel.
     </p>
@@ -28,7 +29,7 @@ const Index=()=>{
     <a href='https://github.com/masummuhammad/trello_ts' style={{color:'blue',textDecoration:'none'}}>github</a> <b>|</b> <a href='https://trello-ts.vercel.app/' style={{color:'blue',textDecoration:'none'}}>link</a>
     <p> </p>
     
-      <a href='/getInTouch' style={{color:'blue',textDecoration:'none'}}>get in touch with me</a> <b>|</b> <a href='https://www.facebook.com/muhammad.masum.98622' style={{color:'blue',textDecoration:'none'}}>knock me on facebook</a>
+      <Link href='/getInTouch' style={{color:'blue',textDecoration:'none'}}>get in touch with me</Link> <b>|</b> <a href='https://www.facebook.com/muhammad.masum.98622' style={{color:'blue',textDecoration:'none'}}>knock me on facebook</a>
     
     </div>
   )
