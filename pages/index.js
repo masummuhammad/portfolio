28a48e1818016711baf22bf0d15706cee08b41c8
Muhammad Masum <48872682+masummuhammad@users.noreply.github.com>
2023-01-11 15:02:16 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,21 @@ import Image from 'next/image';
 const Index=()=>{
   
   return(
-    <div style={{backgroundColor:'#dbe9e2',height:'auto',justifyContent:'center',alignItems:'center',textAlign:'center',fontFamily:'Roboto Mono'}}>
+    <div style={{backgroundColor:'#dbe9e2',height:'auto',justifyContent:'center',alignItems:'center',textAlign:'center',fontFamily:'Roboto Mono',paddingTop:40px,}}>
      <Head>
        <title>Muhammad Masum</title>
       
      </Head>
+      
      <img alt="muhammad masum's photo" src="portfolio_head.jpg" style={{height:'40vh',width:'auto'}}/>
-    <p>Hi, I am Muhammad Masum. I have started my programming journey 5 years ago by a book written by tamim shahriar subeen.It was a 
-      book on c programming language. I was very passionate about to learn how web application works. I 
-      started to learn html, css, javascript, nodejs , sql ,mongodb, express , RESTapi,react, socket.io etc. In 2021, I learnt
-      typescript, graphql , postgresql ,typeorm, nextjs etc and I created <a href='https://tribindu.vercel.app' style={{color:'blue',textDecoration:'none'}}>tribindu</a>.Besides, I can write code in python ,java ,c++ and c++
-       is my favourite.
-      At the beginning I was a self-taught programmer,then I moved to obtain a degree on computer science.
-      I have participated in some online programming contests at codeforces dot com and solved 150+ URI online judge problems.
-      My next step will be solving problems at leetcode.
-      I love to play video games, listen music and to travel.
+    <p>I'm a passionate programmer.I like to solve programming problems and take part in online programming contest.
+    
+    <a href='https://tribindu.vercel.app' style={{color:'blue',textDecoration:'none'}}><b>tribindu</b></a> is an unofficial app to provide BTEB students (dip in engg)results without finding out from pdf documents.
+    And I like typescript...
     </p>
     <h1>Projects</h1>
-    <p><b># tribindu</b> [pdf parsing, web scraping, graphql, react ,express, typescript, javascript, python, postgresql ,typeorm ]</p>
-    <a href='https://tribindu.vercel.app' style={{color:'blue',textDecoration:'none'}}>link</a>
-    <p><b># trello</b> [with react,typescript ,readux ,styled-components](<span style={{color:'red'}}>incomplete</span>)</p>
-    <a href='https://github.com/masummuhammad/trello_ts' style={{color:'blue',textDecoration:'none'}}>github</a> <b>|</b> <a href='https://trello-ts.vercel.app/' style={{color:'blue',textDecoration:'none'}}>link</a>
-    <p> </p>
+    <p><a href='https://tribindu.vercel.app' style={{color:'blue',textDecoration:'none'}}><b># tribindu</b></a> [pdf parsing, web scraping, graphql, react ,express, typescript, javascript, python, postgresql ,typeorm ]</p>
+    
     
       <Link href='/getInTouch' style={{color:'blue',textDecoration:'none'}}>get in touch with me</Link> <b>|</b> <a href='https://www.facebook.com/muhammad.masum.98622' style={{color:'blue',textDecoration:'none'}}>knock me on facebook</a>
     
