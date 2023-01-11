695511b1f562965ff497020b5586da850539495e
Muhammad Masum <48872682+masummuhammad@users.noreply.github.com>
2023-01-11 15:16:24 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,16 @@ import Image from 'next/image';
 const Index=()=>{
   
   return(
-    <div style={{backgroundColor:'#dbe9e2',height:'auto',justifyContent:'center',alignItems:'center',textAlign:'center',fontFamily:'Roboto Mono',paddingTop:40px,}}>
+    <div style={{backgroundColor:'#dbe9e2',height:'auto',justifyContent:'center',alignItems:'center',textAlign:'center',fontFamily:'Roboto Mono',paddingTop:'40px',}}>
      <Head>
        <title>Muhammad Masum</title>
       
      </Head>
       
      <img alt="muhammad masum's photo" src="portfolio_head.jpg" style={{height:'40vh',width:'auto'}}/>
-    <p>I'm a passionate programmer.I like to solve programming problems and take part in online programming contest.
+    <p>I'm a passionate programmer.I like to solve programming problems and take part in online programming contests.
     
+    My app, created with hot technologies.
     <a href='https://tribindu.vercel.app' style={{color:'blue',textDecoration:'none'}}><b>tribindu</b></a> is an unofficial app to provide BTEB students (dip in engg)results without finding out from pdf documents.
     And I like typescript...
     </p>
